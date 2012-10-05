1d7985129c312f1e37ecde3b591455b3523f49e8
Gustavo Duarte <first@duartes.org>
2012-10-04 18:56:50 -0600

    Use file globbing to make life even easier

--- specs/mocha.sh
+++ specs/mocha.sh
@@ -2,6 +2,6 @@
 
 SPECS=`dirname $0`
 MOCHA=$SPECS/../node_modules/mocha/bin/mocha
-echo $MOCHA
+echo Running $SPECS/*$1*
 
-$MOCHA --reporter spec --require should --compilers coffee:coffee-script $SPECS/$1
+$MOCHA --reporter spec --require should --compilers coffee:coffee-script $SPECS/*$1*
