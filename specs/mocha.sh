02dc8992aa55c97ebf56ab47082dc30fac0e85a6
Gustavo Duarte <first@duartes.org>
2012-10-04 18:33:37 -0600

    Add specs/mocha.sh to run specs easily

---
+++ specs/mocha.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SPECS=`dirname $0`
+MOCHA=$SPECS/../node_modules/mocha/bin/mocha
+echo $MOCHA
+
+$MOCHA --reporter spec --require should --compilers coffee:coffee-script $SPECS/$1
+ls -l $MOCHA
+#mocha 
